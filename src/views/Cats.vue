1e8fbbb42837263d82040ce0a9094358cb7bcba8
Jose Zapata <joferzz1989@gmail.com>
2020-03-01 06:33:22 -0400

    Add datatables

--- src/views/Cats.vue
+++ src/views/Cats.vue
@@ -1,12 +1,73 @@
 <template>
   <div>
-    Cats
+    <h1>Cats for Adoption</h1>
+    <hr>
+    <v-data-table
+      :headers="headers"
+      :items="cats"
+      :items-per-page="5"
+      class="elevation-1"
+    ></v-data-table>
   </div>
 </template>
 
 <script>
+import cats from '@/data/cats'
 export default {
-  name: 'Cats'
+  name: 'Cats',
+  data () {
+    return {
+      headers: [
+        {
+          text: 'Pet name',
+          align: 'start',
+          sortable: false,
+          value: 'name'
+        },
+        {
+          text: 'Breed',
+          align: 'start',
+          value: 'breed'
+        },
+        {
+          text: 'Species',
+          align: 'start',
+          value: 'species'
+        },
+        {
+          text: 'Gender',
+          align: 'start',
+          value: 'gender'
+        },
+        {
+          text: 'Age',
+          align: 'start',
+          value: 'age'
+        },
+        {
+          text: 'Color',
+          align: 'start',
+          value: 'color'
+        },
+        {
+          text: 'Weight',
+          align: 'start',
+          value: 'weight'
+        },
+        {
+          text: 'Location',
+          align: 'start',
+          value: 'location'
+        },
+        {
+          text: 'Notes',
+          align: 'start',
+          value: 'notes'
+        }
+      ],
+      cats
+    }
+  }
 }
 </script>
 
--- src/views/Dogs.vue
+++ src/views/Dogs.vue
@@ -1,12 +1,66 @@
 <template>
   <div>
-    Dogs
+    <v-data-table
+      :headers="headers"
+      :items="dogs"
+      :items-per-page="5"
+      class="elevation-1"
+    ></v-data-table>
   </div>
 </template>
 
 <script>
+import dogs from '@/data/dogs'
 export default {
-  name: 'Dogs'
+  name: 'Dogs',
+  data () {
+    return {
+      headers: [
+        {
+          text: 'Pet name',
+          align: 'start',
+          sortable: false,
+          value: 'name'
+        },
+        {
+          text: 'Breed',
+          align: 'start',
+          value: 'breed'
+        },
+        {
+          text: 'Gender',
+          align: 'start',
+          value: 'gender'
+        },
+        {
+          text: 'Age',
+          align: 'start',
+          value: 'age'
+        },
+        {
+          text: 'Color',
+          align: 'start',
+          value: 'color'
+        },
+        {
+          text: 'Weight',
+          align: 'start',
+          value: 'weight'
+        },
+        {
+          text: 'Location',
+          align: 'start',
+          value: 'location'
+        },
+        {
+          text: 'Notes',
+          align: 'start',
+          value: 'notes'
+        }
+      ],
+      dogs
+    }
+  }
 }
 </script>
 
